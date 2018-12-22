eeb38377eedc817c0395e591750e2cb4348703e8
Polska Panda <cookavsky@gmail.com>
2018-12-22 13:18:56 +0100

    Recreate

---
+++ #1 WWC Challange/js/script.js
@@ -0,0 +1,17 @@
+const Check = document.getElementsByClassName("Check");
+
+const AcceptCheck = function() {
+    for(let i = 0; i < Check.length; i++) {
+        Check[i].addEventListener("click", function() {
+            if(Check[i].className === "Check red") {
+                Check[i].classList.remove("red");
+                Check[i].classList.add("green");
+            } else if(Check[i].className === "Check green") {
+                Check[i].classList.remove("green");
+                Check[i].classList.add("red");
+            }
+        })
+    }
+}
+
+AcceptCheck();
