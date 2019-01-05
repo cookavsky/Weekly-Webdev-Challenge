e6c95183e9b425786d5eb8762c1b17ffc862e234
Marcin Kukawski <cookavsky@gmail.com>
2019-01-05 20:27:39 +0100

    Update config.php

--- #1 WWC Challange/config.php
+++ #1 WWC Challange/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'mysql32.mydevil.net');
-define('DB_USERNAME', 'm11737');
-define('DB_PASSWORD', 'Kuku648112');
-define('DB_NAME', 'm11737_Userbases');
+define('DB_SERVER');
+define('DB_USERNAME');
+define('DB_PASSWORD');
+define('DB_NAME');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
