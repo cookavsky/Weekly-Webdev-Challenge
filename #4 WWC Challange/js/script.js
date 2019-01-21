72c05799ccbc3b0af78180248b463bafbf3f9714
Polska Panda <cookavsky@gmail.com>
2019-01-21 23:19:14 +0100

    C

--- #4 WWC Challange/js/script.js
+++ #4 WWC Challange/js/script.js
@@ -11,3 +11,23 @@ $(document).ready(function () {
     });
 
 });
+
+$(document).ready(function () {
+    $("#Menu-B-T").on('click', function () {
+        $("#MiniMenu-B-T").toggleClass("off on");
+        $(".MiniBar-B-T").toggleClass("off on");
+    });
+    $("#Menu-B-E").on('click', function () {
+        $("#MiniMenu-B-E").toggleClass("off on");
+        $(".MiniBar-B-E").toggleClass("off on");
+    });
+    $("#Menu-B-B").on('click', function () {
+        $("#MiniMenu-B-B").toggleClass("off on");
+        $(".MiniBar-B-B").toggleClass("off on");
+    });
+    $("#Menu-B-L").on('click', function () {
+        $("#MiniMenu-B-L").toggleClass("off on");
+        $(".MiniBar-B-L").toggleClass("off on");
+    });
+
+});
